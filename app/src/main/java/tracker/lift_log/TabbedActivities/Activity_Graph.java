6e0c3724261c180ba43552fa3cb33de4a690a2cb
Chase Burnham <cburnhamiv@gmail.com>
2016-06-16 14:47:10 -0400

    Fixed graph

--- app/src/main/java/tracker/lift_log/TabbedActivities/Activity_Graph.java
+++ app/src/main/java/tracker/lift_log/TabbedActivities/Activity_Graph.java
@@ -7,12 +7,15 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.PointsGraphSeries;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -31,17 +34,19 @@ public class Activity_Graph extends Activity{
     String maxDate;
 
     private GraphView graph;
+    private RelativeLayout rel_graph;
+    private TextView tvNoData;
 
-    LineGraphSeries <DataPoint> oneMonthSeries;
-    LineGraphSeries <DataPoint> threeMonthSeries;
-    LineGraphSeries <DataPoint> sixMonthSeries;
-    LineGraphSeries <DataPoint> oneYearSeries;
-    LineGraphSeries <DataPoint> allTimeSeries;
+    /* DataPoints and series */
+    private LineGraphSeries<DataPoint> lineGraphSeries;
+    private ArrayList<DataPoint> dataPoints;
 
     LiftDatabaseHelper liftDatabaseHelper;
     private int lid;
 
     private AdsHelper adsHelper;
+
+    TextView[] tvDateSelections;
 
     /* todo add message for not enough data */
 
@@ -52,8 +57,10 @@ public class Activity_Graph extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph_progress);
 
+        rel_graph = (RelativeLayout) findViewById(R.id.rel_graph);
+        tvNoData = (TextView) findViewById(R.id.tv_noData);
+
         liftDatabaseHelper = new LiftDatabaseHelper(getBaseContext());
-        SQLiteDatabase db = liftDatabaseHelper.getReadableDatabase();
 
         Intent recievedIntent = getIntent();
         lid = recievedIntent.getIntExtra("LID", 0);
@@ -62,7 +69,6 @@ public class Activity_Graph extends Activity{
         graph = (GraphView) findViewById(R.id.graph);
         //------------------------------------//
         //CALL PRIVATE METHODS TO CREATE GRAPH
-        ArrayList<Integer> maxes = null;
         try {
             this.getDataPoints();
         } catch (ParseException e) {
@@ -70,14 +76,7 @@ public class Activity_Graph extends Activity{
         }
 
 
-//        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(dps);
-//        if (maxes.size()>0){
-//            this.createGraph(series, maxes.get(0));
-//        }
-
-        this.setUpTextViews();
-
-
+        this.setupDateSelections();
         TextView maxView = (TextView) findViewById(R.id.Max);
         TextView occuredOnView = (TextView) findViewById(R.id.Occured_On);
         double max = getCalculatedMax();
@@ -86,7 +85,7 @@ public class Activity_Graph extends Activity{
             occuredOnView.setText("Occured on: "+ dc.convertDateToText(maxDate));
         }
 
-        this.createGraph(oneMonthSeries, 0);
+        this.createGraph();
 
         adsHelper = new AdsHelper(getWindow().findViewById(android.R.id.content), getResources().getString(R.string.banner_ad_on_pastlifts),this);
         adsHelper.setUpAds();
@@ -102,176 +101,153 @@ public class Activity_Graph extends Activity{
 
     }
 
+    public class OnDateRangeSelection implements View.OnClickListener{
+        private double timeLimit;
+        private int tvIndex;
+        public OnDateRangeSelection(double timeLimit, int tvIndex) {
+            this.timeLimit = timeLimit;
+            this.tvIndex = tvIndex;
+        }
+
+        @Override
+        public void onClick(View v) {
+            Date currentDate = new Date();
+            final double currentTime = currentDate.getTime();
+
+            ArrayList<DataPoint> dataPointsLocal = new ArrayList<DataPoint>();
+                /* Iterate though the global datapoints to get the ones that fall in a 1 month range */
+            for (DataPoint dataPoint: dataPoints){
+                if(currentTime-dataPoint.getX()<= timeLimit){
+                    dataPointsLocal.add(dataPoint);
+                }
+            }
+
+            DataPoint[] dataPoints1 = dataPointsLocal.toArray(new DataPoint[dataPointsLocal.size()]);
+            lineGraphSeries.resetData(dataPoints1);
+            Viewport viewport = graph.getViewport();
+            viewport.setMinX(lineGraphSeries.getLowestValueX()-5*24*60*60*1000);
+            viewport.setMaxX(lineGraphSeries.getHighestValueX()+5*24*60*60*1000);
+            viewport.setMinY(lineGraphSeries.getLowestValueY()-5);
+            viewport.setMaxY(lineGraphSeries.getHighestValueY()+5);
+            /* Set all points textviews to null bg */
+            for (TextView tv: tvDateSelections){
+                tv.setBackgroundColor(0);
+            }
+            /* set background for selected item */
+            tvDateSelections[tvIndex].setBackgroundColor(getResources().getColor(R.color.tv_graph_background));
+        }
+    }
 
     private void getDataPoints() throws ParseException {
         SQLiteDatabase db = liftDatabaseHelper.getReadableDatabase();
 
         /* Run the sql command */
-        String sql = "SELECT MAX(maxWeight), date_Lifted FROM Max WHERE lid = "+lid +" GROUP BY date_Lifted ORDER BY date_Lifted";
+        String sql = "SELECT MAX(maxWeight), date_Lifted FROM Max WHERE lid = "+lid
+                +" GROUP BY date_Lifted ORDER BY date_Lifted";
         Cursor c = db.rawQuery(sql, null);
 
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
-        oneMonthSeries = new LineGraphSeries<>();
-        threeMonthSeries = new LineGraphSeries<>();
-        sixMonthSeries = new LineGraphSeries<>();
-        oneYearSeries = new LineGraphSeries<>();
-        allTimeSeries = new LineGraphSeries<>();
+        dataPoints = new ArrayList<>();
+        lineGraphSeries = new LineGraphSeries<>();
 
         c.moveToFirst();
 
-        /* Get the current date */
-        Date currentDate = new Date();
-
-        double currentTime = currentDate.getTime();
-        Log.e("CURRENT TIME ", " current time = " + currentTime);
-        /* Milliseconds in a day */
-        double timeInDay = 24 * 60 * 60 * 1000;
-        double timeInMonth = timeInDay * 30.5;
-        double timeIn3Month = timeInMonth *3;
-        double timeIn6Month = timeIn3Month * 2;
-        double timeInYear = timeIn6Month * 2;
-
-        for(int i = 0; !c.isAfterLast() ; i++){
+                /* Add the maxes to the line series */
+        while(!c.isAfterLast()){
             Date date = dateFormat.parse(c.getString(1));
-            double timeDiff  = currentTime - date.getTime();
             DataPoint dataPoint =  new DataPoint(date, c.getInt(0));
-            allTimeSeries.appendData(dataPoint, true, c.getCount());
-            Log.e("CURRENT TIME ", " date time = " + date.getTime());
-            Log.e("DIFF TIME ", " diff time = "+timeDiff);
-            if(timeDiff < timeInYear){
-                oneYearSeries.appendData(dataPoint, true, c.getCount());
-                if(timeDiff < timeIn6Month){
-                    sixMonthSeries.appendData(dataPoint, true, c.getCount());
-                    if(timeDiff < timeIn3Month){
-                        threeMonthSeries.appendData(dataPoint, true, c.getCount());
-                        if(timeDiff < timeInMonth){
-                            oneMonthSeries.appendData(dataPoint, true, c.getCount());
-                            Log.e("TIME DIFF", "Days dif = " + timeDiff/timeInDay);
-                        }
-                    }
-                }
-            }
-
+            dataPoints.add(dataPoint);
+            lineGraphSeries.appendData(dataPoint, true, c.getCount());
             c.moveToNext();
         }
 
+        if(dataPoints.size() == 0){
+            tvNoData.setVisibility(View.VISIBLE);
+            rel_graph.setVisibility(View.GONE);
+        }else{
+            tvNoData.setVisibility(View.GONE);
+            rel_graph.setVisibility(View.VISIBLE);
+        }
+
         c.close();
+        db.close();
     }
 
-    private void setUpTextViews(){
+
+    /* todo clean up code */
+    public void setupDateSelections(){
         final TextView tv_1m = (TextView) findViewById(R.id.tv_1m);
         final TextView tv_3m = (TextView) findViewById(R.id.tv_3m);
         final TextView tv_6m = (TextView) findViewById(R.id.tv_6m);
         final TextView tv_1y = (TextView) findViewById(R.id.tv_1y);
         final TextView tv_all = (TextView) findViewById(R.id.tv_all);
 
+        tvDateSelections = new TextView[]{tv_1m, tv_3m, tv_6m, tv_1y, tv_all};
 
-        tv_1m.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                createGraph(oneMonthSeries, 0);
-                tv_1m.setBackgroundColor(getResources().getColor(R.color.tv_graph_background));
-                tv_3m.setBackgroundColor(0);
-                tv_6m.setBackgroundColor(0);
-                tv_1y.setBackgroundColor(0);
-                tv_all.setBackgroundColor(0);
-            }
-        });
-        tv_3m.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                createGraph(threeMonthSeries, 0);
-                tv_1m.setBackgroundColor(0);
-                tv_3m.setBackgroundColor(getResources().getColor(R.color.tv_graph_background));
-                tv_6m.setBackgroundColor(0);
-                tv_1y.setBackgroundColor(0);
-                tv_all.setBackgroundColor(0);
-            }
-        });
-        tv_6m.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        /* Milliseconds in a day */
+        double timeInDay = 24 * 60 * 60 * 1000;
+        final double timeInMonth = timeInDay * 30.5;
+        final double timeIn3Month = timeInMonth *3;
+        final double timeIn6Month = timeIn3Month * 2;
+        final double timeInYear = timeIn6Month * 2;
 
-                /* todo remove masxifOne */
-                createGraph(sixMonthSeries, 0);
+        /* todo check if you can break if not true */
+        tv_1m.setOnClickListener(new OnDateRangeSelection(timeInMonth, 0));
+        tv_3m.setOnClickListener(new OnDateRangeSelection(timeIn3Month, 1));
+        tv_6m.setOnClickListener(new OnDateRangeSelection(timeIn6Month, 2));
+        tv_1y.setOnClickListener(new OnDateRangeSelection(timeInYear, 3));
+        tv_all.setOnClickListener(new OnDateRangeSelection(Double.MAX_VALUE, 4));
 
-                tv_1m.setBackgroundColor(0);
-                tv_3m.setBackgroundColor(0);
-                tv_6m.setBackgroundColor(getResources().getColor(R.color.tv_graph_background));
-                tv_1y.setBackgroundColor(0);
-                tv_all.setBackgroundColor(0);
-            }
-        });
-        tv_1y.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                createGraph(oneYearSeries, 0);
-
-                tv_1m.setBackgroundColor(0);
-                tv_3m.setBackgroundColor(0);
-                tv_6m.setBackgroundColor(0);
-                tv_1y.setBackgroundColor(getResources().getColor(R.color.tv_graph_background));
-                tv_all.setBackgroundColor(0);
-            }
-        });
-        tv_all.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /* todo remove masxifOne */
-                createGraph(allTimeSeries, 0);
-
-                tv_1m.setBackgroundColor(0);
-                tv_3m.setBackgroundColor(0);
-                tv_6m.setBackgroundColor(0);
-                tv_1y.setBackgroundColor(0);
-                tv_all.setBackgroundColor(getResources().getColor(R.color.tv_graph_background));
-            }
-        });
     }
 
-    /* todo just change the x min and x max for times */
-    private void createGraph(LineGraphSeries<DataPoint> series, int maxIfOne){
-
-
+    private void createGraph(){
+        graph.setTitle("Max Weight Over Time");
         graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this));
-        if(!series.isEmpty()){
+        if(!lineGraphSeries.isEmpty()){
             graph.getGridLabelRenderer().setNumHorizontalLabels(3);
             graph.getViewport().setXAxisBoundsManual(true);
+            graph.getViewport().setYAxisBoundsManual(true);
+            if(dataPoints.size() == 1){
+                DataPoint dataPoint = dataPoints.get(0);
+                PointsGraphSeries<DataPoint> seriesSingle = new PointsGraphSeries<DataPoint>(new DataPoint[] {
+                        dataPoint
+                });
 
-//            if(series.) == 1){
-//                PointsGraphSeries<DataPoint> series3 = new PointsGraphSeries<DataPoint>(new DataPoint[] {
-//                        new DataPoint(dates.get(0), maxIfOne),
-//                });
-//
-//                graph.addSeries(series3);
-//                series3.setShape(PointsGraphSeries.Shape.TRIANGLE);
-//
-//                graph.getViewport().setMinX(dates.get(0).getTime()-5*24*60*60*1000);
-//                graph.getViewport().setMaxX(dates.get(dates.size()-1).getTime()+5*24*60*60*1000);
-//                graph.getViewport().setYAxisBoundsManual(true);
-//                graph.getViewport().setMinY(maxIfOne-10);
-//                graph.getViewport().setMaxY(maxIfOne + 10);
-//                graph.setTitle("Max Weight Over Time");
-            //}else{
-                graph.getViewport().setMinX(series.getLowestValueX()-.5*24*60*60*1000);
-                graph.getViewport().setMaxX(series.getHighestValueX());
-                graph.setTitle("Max Weight Over Time");
+                graph.addSeries(seriesSingle);
+                seriesSingle.setShape(PointsGraphSeries.Shape.TRIANGLE);
 
-                series.setDrawDataPoints(true);
-                series.setDataPointsRadius(10);
-                series.setThickness(4);
+                Viewport viewport = graph.getViewport();
+                viewport.setMinX(dataPoint.getX()-5*24*60*60*1000);
+                viewport.setMaxX(dataPoint.getX()+5*24*60*60*1000);
 
-                graph.getViewport().setScalable(true);
-                graph.getViewport().setScrollable(true);
-                graph.addSeries(series);
-            //}
+                viewport.setMinY(dataPoint.getY()-10);
+                viewport.setMaxY(dataPoint.getY() + 10);
+
+            }else{
+                Viewport viewport = graph.getViewport();
+                viewport.setMinX(lineGraphSeries.getLowestValueX()-5*24*60*60*1000);
+                viewport.setMaxX(lineGraphSeries.getHighestValueX()+5*24*60*60*1000);
+                viewport.setMinY(lineGraphSeries.getLowestValueY()-5);
+                viewport.setMaxY(lineGraphSeries.getHighestValueY()+5);
+
+
+                lineGraphSeries.setDrawDataPoints(true);
+                lineGraphSeries.setDataPointsRadius(10);
+                lineGraphSeries.setThickness(4);
+
+                //graph.getViewport().setScalable(true);
+                //graph.getViewport().setScrollable(true);
+                graph.addSeries(lineGraphSeries);
+            }
 
 
         }
 
     }
+
+
+
 
     private double getCalculatedMax() {
         SQLiteDatabase db = liftDatabaseHelper.getReadableDatabase();
